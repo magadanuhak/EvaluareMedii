ca0bb52e7e277fda726fbf3fbce6b2a09a55d18e
zalevschi <zalevschi1998@gmail.com>
2019-05-14 10:31:18 +0300

    all

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#testBtn').addEventListener('click', function () {
         alert('Buton test ');
+        var xmlhttp = new XMLHttpRequest();
+        var url = "http://localhost:3000/posts";
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var myArr = JSON.parse(this.responseText);
+                myFunction(myArr);
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
     });
+    function myFunction(arr) {
+        var out = "";
+        var i;
+        for (i = 0; i < arr.length; i++) {
+            out += '<a href="' + arr[i].url + '">' +
+                arr[i].display + '</a><br>';
+        }
+        document.getElementById("id01").innerHTML = out;
+    }
 });

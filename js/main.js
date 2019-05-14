fc32e2f8c15a615ac117112a030d9cfaeacf4ee1
zalevschi <zalevschi1998@gmail.com>
2019-05-14 09:57:43 +0300

    ok

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#testBtn').addEventListener('click', function () {
+        alert('Buton test ');
+    });
+});
